434e0ad348c3342bc9a30533569dabbd14a3f55b
loponavt <71023024+loponavt@users.noreply.github.com>
2024-03-11 20:39:52 +0300

    Добавил логгер и отслеживание цен

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3 as sq
+import main
 
-db = sq.connect('telgb.db')
+db = sq.connect('telgb.db', check_same_thread=False)
 cur = db.cursor()
 
 async def start_db():
@@ -8,16 +9,33 @@ async def start_db():
         cur.execute('CREATE TABLE IF NOT EXISTS data '
                     '(tg_id INTEGER, '
                     'article TEXT, '
-                    'current_price INTEGER)')
+                    'price INTEGER)')
         db.commit()
-        print("db started")
+        main.file_logger.info('db started')
+        main.console_logger.info('db_started')
 
-async def add_article(tg_id, article, current_price):
+async def add_article(tg_id, article, price, first_name, last_name, username):
     with db:
         if not cur.execute('SELECT tg_id, article FROM data WHERE tg_id = ? AND article = ?', (tg_id, article)).fetchone():
-            cur.execute("INSERT INTO data (tg_id, article, current_price) "
-                        "VALUES (?, ?, ?)", (tg_id, article, current_price))
+            cur.execute("INSERT INTO data (tg_id, article, price) "
+                        "VALUES (?, ?, ?)", (tg_id, article, price))
             db.commit()
-            print("article added")
+            main.file_logger.info(f"{article} added by {first_name} {last_name} {username}")
         else:
-            print("this article from that user already is added")
+            main.file_logger.info("this article from that user already added")
+
+async def price_checker():
+    with db:
+        for data in cur.execute('SELECT article, price, tg_id FROM data').fetchall():
+            price_in_table = data[1]
+            article = data[0]
+            tg_id = data[2]
+            answer = await main.get_item_data(article)
+            item_name = answer['name']
+            supplier = answer['supplier']
+            if price_in_table>answer['price']:
+                await main.write_to_user(tg_id, f"`{article}`\n{item_name}\n{supplier}\n"
+                                         f"{price_in_table} ---> {answer['price']}")
+                cur.execute("UPDATE data SET price = ? WHERE article = ?",
+                            (answer['price'], article))
+                db.commit()
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import asyncio
 import logging
+import requests
+
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
-from aiogram.types import Message, ReplyKeyboardMarkup
+from aiogram.types import Message
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 import database as db
-import requests
+
 from fake_useragent import UserAgent
 import os
 from dotenv import load_dotenv
@@ -12,17 +16,19 @@ from dotenv import load_dotenv
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
+file_logger = logging.getLogger('file_logger')
+console_logger = logging.getLogger('console_logger')
 
 
 async def get_item_data(article: str):
-    headers = {'user-agent': UserAgent(use_external_data=True).chrome}
+    headers = {'user-agent': UserAgent().chrome}
     response = requests.get(url=f'https://card.wb.ru/cards/detail'
                                 f'?spp=18&locale=ru&lang=ru&curr=rub'
                                 f'&nm={article}', headers=headers)
     answer = {'name': '',
               'supplier': '',
               'price': 0
-    }
+              }
     try:
         response.raise_for_status()
         json_data = response.json().get('data')
@@ -30,25 +36,27 @@ async def get_item_data(article: str):
         if json_data:
             for product in json_data.get('products'):
                 answer['name'] = product.get('name')
-                answer['price'] = round(product.get('salePriceU')/100)
+                answer['price'] = round(product.get('salePriceU') / 100)
                 answer['supplier'] = product.get('supplier')
-                review_rating = product.get('reviewRating')
-                # TODO хорошо бы возвращать еще и картинку
-
+                # TODO хорошо бы возвращать еще и картинку но дорого
     except requests.exceptions.HTTPError:
-        logging.warning(
-            'Проблемы с получением данных, '
-            f'артикул {article}')
+        file_logger.warning(f'Проблемы с получением данных, артикул {article}')
     except AttributeError as e:
-        logging.error(
-            'Ошибка при обработке данных '
-            f'для артикула {article}: {e}')
+        file_logger.error(f'Ошибка при обработке данных для артикула {article}: {e}')
     return answer
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer(f'Привет {message.from_user.first_name}\n'
                          f'Для отслеживания цены товара, пришли мне его артикул')
+
+async def start_looper():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(db.price_checker, trigger='interval', seconds=60)
+    scheduler.start()
+    file_logger.info('looper started')
+    console_logger.info('looper started')
+
 
 @dp.message(F.text)
 async def message_handler(message: Message):
@@ -58,16 +66,46 @@ async def message_handler(message: Message):
     item_price = item_data['price']
     if item_data['name'] == '':
         answer = 'Не могу найти такой артикул'
+        file_logger.info(f'{message.from_user.first_name} {message.from_user.last_name} '
+                         f'@{message.from_user.username}: {message.text}')
     else:
-        answer = f'{item_name}\n{item_supplier}\n{item_price} ₽'
+        answer = f'{item_name}\n{item_supplier}\n{item_price} ₽\nТовар добавлен'
     await message.answer(answer)
     if answer != 'Не могу найти такой артикул':
-        await db.add_article(message.from_user.id, message.text, item_price)
+        await db.add_article(message.from_user.id, message.text, item_price,
+                             message.from_user.first_name, message.from_user.last_name, message.from_user.username)
+
+
+async def write_to_user(chat_id, text: str):
+    await bot.send_message(chat_id, text, parse_mode="MARKDOWN")
+
 
 async def main():
-    await db.start_db()
-    await dp.start_polling(bot)
+    await asyncio.gather(db.start_db(),
+                         start_looper(),
+                         dp.start_polling(bot))
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)s] >> %(message)s',
+                                  datefmt='%d.%m.%y %H:%M:%S')
+    file_logger.setLevel(logging.INFO)
+    console_logger.setLevel(logging.INFO)
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    console_logger.addHandler(sh)
+
+    if not os.path.isdir('log_dir'):
+        os.makedirs('log_dir')
+        console_logger.info('create log_dir')
+
+    fh = logging.FileHandler('log_dir/logi.log')
+    fh.setFormatter(formatter)
+    file_logger.addHandler(fh)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main())
+    except (KeyboardInterrupt, SystemExit):
+        pass
